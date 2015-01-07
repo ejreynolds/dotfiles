aa250236029d78ec816257fefbd11d3a6e0dce48
Erik Reynolds <erik@reynolds.io>
2015-01-07 02:31:59 -0500

    More tweaks...

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -20,9 +20,9 @@ git_dirty() {
   else
     if [[ $($git status --porcelain) == "" ]]
     then
-      echo "%{$fg_bold[green]%}$(git_prompt_info) → %{$reset_color%}"
+      echo "%{$fg_bold[green]%}$(git_prompt_info)%{$fg[yellow]%} → %{$reset_color%}"
     else
-      echo "%{$fg_bold[red]%}$(git_prompt_info) → %{$reset_color%}"
+      echo "%{$fg_bold[red]%}$(git_prompt_info)%{$fg[yellow]%} → %{$reset_color%}"
     fi
   fi
 }
